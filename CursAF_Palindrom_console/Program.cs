85940663106eb0b8775af638fbe9e583a6e02919
ca7a88 <rotariucatalin2001@gmail.com>
2024-12-23 20:12:10 +0200

    Add files via upload
    Afiseaza decat numerele palindrom dintr-un vector de numere

---
+++ CursAF_Palindrom_console/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursAF_Palindrom_console
+{
+    // Afiseaza decat numerele palindrom dintr-un vector de numere
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] data = Console.ReadLine().Split(' ');
+
+            int[] v = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(data[i]);
+                if (IsPalindrome(v[i]))
+                    Console.Write(v[i] + " ");
+            }
+        }
+
+        public static bool IsPalindrome(int n)
+        {
+            int oglindit = 0, copie_n = n;
+
+            while (n != 0)
+            {
+                oglindit = oglindit * 10 + n % 10;
+                n = n / 10;
+            }
+
+            if (oglindit == copie_n)
+                return true;
+            return false;
+        }
+    }
+}
